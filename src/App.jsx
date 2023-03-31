f27197c72607c65e5a221ab67bd71ad973efccd4
Sumona Akter Shimu <sumona59shimu@gmail.com>
2023-03-31 11:22:14 +0600

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App text-bg-danger">
-      <h1>welcome to our Thoughful Quest</h1>
+    <div className="App container">
+      <Navbar></Navbar>
     </div>
   )
 }
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className="navbar bg-body-success">
+            <div className="container-fluid border-bottom py-3" >
+                <a className="navbar-brand" href="#">
+                    <img src="https://clutchco-static.s3.amazonaws.com/s3fs-public/logos/tq_design_logo.png?VersionId=sxxLchfesHB41oJHRJDDyNKbUHqhu5mZ" alt="Logo" width="30" height="24"
+                        className="d-inline-block align-text-top" />
+                    Thoughtful Quest
+                </a>
+                <div style={{ height: '50px', width: '50px' }}>
+                    <img className='img-fluid rounded-circle' src="https://randomuser.me/api/portraits/men/22.jpg" alt="" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
