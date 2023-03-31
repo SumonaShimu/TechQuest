995babbec6f9af2d95b3131103c7579d5afb3fc0
Sumona Akter Shimu <sumona59shimu@gmail.com>
2023-03-31 15:03:03 +0600

    bookmark icon hover effect added, blog structure complete

--- src/Components/Body/Blog/Blog.jsx
+++ src/Components/Body/Blog/Blog.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './Blog.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBookmark, faBookReader, faCoffee } from '@fortawesome/free-solid-svg-icons'
+
 const Blog = ({ blog }) => {
     const { id, cover, author, title, name, time } = blog;
     return (
         <div className='blogs container rounded mb-4 pb-4'>
-            <img className='img-fluid m-2 rounded' src={cover} alt="cover" />
+            <img className='w-100 m-2 rounded' src={cover} alt="cover" />
             <h1>{title}</h1>
 
-            <div style={{display:'flex',justifyContent:'space-between',alignItems:'center'}}>
+            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
                 <div className='author-container my-5'>
                     <img className='img-fluid rounded-circle' src={author} alt="author" />
                     <div>
@@ -15,13 +18,19 @@ const Blog = ({ blog }) => {
                         <small className='grey'>Mar 14 (4 Days ago)</small>
                     </div>
                 </div>
-                <small className='grey'>{time} min read</small>
+                <small className='grey'>
+                    {time} min read
+                    <button className='bookmark-btn'><FontAwesomeIcon icon={faBookmark} color="#016969" size="lg" className='hov'/></button>
+                </small>
             </div>
 
             <small className='grey'>#programming </small>
             <small className='grey'>#beginners </small>
 
-            <button className='d-block w-100 btn btn-light'>Mark as Read</button>
+            <button className='d-block w-100 btn btn-light'>Mark as Read
+            <FontAwesomeIcon icon={faBookReader} color="#016969" className='ms-2'/>
+            
+            </button>
         </div>
     );
 };
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
                 <a className="navbar-brand" href="#">
                     <img src="https://clutchco-static.s3.amazonaws.com/s3fs-public/logos/tq_design_logo.png?VersionId=sxxLchfesHB41oJHRJDDyNKbUHqhu5mZ" alt="Logo" width="30" height="30"
                         className="d-inline-block align-text-top mx-2" />
-                     Thoughtful Quest
+                     Thought<span style={{color:'red'}}>ful</span> <span style={{color:'green'}}>Quest</span>
                 </a>
                 <div style={{ height: '50px', width: '50px' }}>
                     <img className='img-fluid rounded-circle' src="https://randomuser.me/api/portraits/men/22.jpg" alt="user" />
